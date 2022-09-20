13c5a9127908b68fcd9208a06111fa1be407929e
Darief Maes <40837996+DariefMaes@users.noreply.github.com>
2022-09-20 13:36:31 -0700

    Update fetchInfo.ts

--- utils/fetchInfo.ts
+++ utils/fetchInfo.ts
@@ -1,8 +1,8 @@
 import { Info } from "../typings";
 
 export const fetchInfo = async () => {
-
-  const res = await fetch('https://debately-soon-5wljr35tr-dariefmaes.vercel.app');
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getInfo`);
 
   const data = await res.json();
   const info: Info[] = data.info;
