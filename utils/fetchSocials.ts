0a235dad18b574e9ef82a25a8ea1c22797d3de00
Darief Maes <40837996+DariefMaes@users.noreply.github.com>
2022-09-20 13:36:14 -0700

    Update fetchSocials.ts

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,7 +1,9 @@
 import { Social } from "../typings";
 
 export const fetchSocials = async () => {
-  const res = await fetch('https://debately-soon-5wljr35tr-dariefmaes.vercel.app');
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getSocials`
+  );
 
   const data = await res.json();
   const socials: Social[] = data.socials;
