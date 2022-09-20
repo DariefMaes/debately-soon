9572dcfb76d2b0fc353358da47329a445d89fcf6
Darief Maes <40837996+DariefMaes@users.noreply.github.com>
2022-09-20 13:01:37 -0700

    Update fetchInfo.ts

--- utils/fetchInfo.ts
+++ utils/fetchInfo.ts
@@ -1,7 +1,7 @@
 import { Info } from "../typings";
 
 export const fetchInfo = async () => {
-  const res = await fetch("/api/getInfo");
+  const res = await fetch(`${process.env.VERCEL_URL}/api/getInfo`);
 
   const data = await res.json();
   const info: Info[] = data.info;
