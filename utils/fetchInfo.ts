d0b2f6490e86d432adf5ea86a0f256c9fdba5ea7
Darief Maes <dariefmaes@gmail.com>
2022-09-20 13:25:12 -0700

    replace URL

--- utils/fetchInfo.ts
+++ utils/fetchInfo.ts
@@ -1,7 +1,10 @@
 import { Info } from "../typings";
 
 export const fetchInfo = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getInfo`);
+  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL;
+  const Url = "/api/getInfo";
+
+  const res = await fetch(new URL(Url, baseURL));
 
   const data = await res.json();
   const info: Info[] = data.info;
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,9 +1,10 @@
 import { Social } from "../typings";
 
 export const fetchSocials = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getSocials`
-  );
+  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL;
+  const Url = "/api/getSocials";
+
+  const res = await fetch(new URL(Url, baseURL));
 
   const data = await res.json();
   const socials: Social[] = data.socials;
