aea16b77614cd43a356d37da3f661eb4b576ec39
Darief Maes <40837996+DariefMaes@users.noreply.github.com>
2022-09-20 14:03:08 -0700

    Update fetchInfo.ts

--- utils/fetchInfo.ts
+++ utils/fetchInfo.ts
@@ -1,8 +1,13 @@
 import { Info } from "../typings";
 
 export const fetchInfo = async () => {
-  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
-  const res = await fetch(`${process.env.VERCEL_URL}/api/getInfo`);
+  const dev = process.env.NODE_ENV !== "production";
+
+  const server = dev
+    ? "http://localhost:3000"
+    : process.env.NEXT_PUBLIC_VERCEL_URL;
+
+  const res = await fetch(`${server}/api/getInfo`);
 
   const data = await res.json();
   const info: Info[] = data.info;
